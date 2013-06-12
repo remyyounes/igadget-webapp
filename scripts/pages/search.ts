3b1626fafb945b486739d3840e073d3a3f1e573b
Naseem Makiya <naseem.makiya@moovweb.com>
2013-06-12 14:17:48 -0700

    search results tritium finished

--- scripts/pages/search.ts
+++ scripts/pages/search.ts
@@ -1,7 +1,16 @@
 $("body") {
   # Transform product page html
   insert("div", data-role:"page", id:"search") {
-    move_here("/html/body//div[@id='_header']")
+    move_here("/html/body//div[@id='_header']") {
+      $$("._logo") {
+        add_class("_back")
+        $$(".sprites-logo") {
+          name("a")
+          attributes(data-rel:"back", data-role:"button")
+          insert("span", "BACK", class:"_back_text")
+        }
+      }
+    }
     insert("div", class:"_content", data-role:"content") {
       move_here("/html/body//div[@id='Container']") {
         # Make product images into jqm buttons
@@ -10,6 +19,44 @@ $("body") {
             attribute("data-role", "button")
           }
         }
+
+        # remove content we don't need
+        $$(".AdvancedSearch, #SearchTabsList, #SearchResultsBrand,
+            .SearchSorting, #SearchResultsCategoryAndBrand, .ToggleSearchFormLink") {
+          remove()
+        }
+
+        $$("h2") {
+          add_class("_bar_header")
+        }
+
+        $$(".ProductList") {
+          $$("li") {
+            add_class("_flex_box")
+            $$("ProductImage") {
+              add_class("_flex_box_item_3")
+            }
+            $$("ProductDetails") {
+              add_class("_flex_box_item_1")
+            }
+            $$(".ProductQty") {
+              remove()
+            }
+            $$("strong") {
+              move_to("..", "top")
+            }
+            $$(".ProductCompareButton") {
+              remove()
+            }
+          }
+        }
+
+        $$(".CompareButtonContainer, .FeedLink") {
+          remove()
+        }
+        
+        remove(".//br")
+
       }
     }
     move_here("/html/body//div[@id='_footer']")
