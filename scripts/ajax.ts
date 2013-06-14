4b2b1459e426352d921db29ede0b76211e7d593e
Naseem Makiya <naseem.makiya@moovweb.com>
2013-06-14 16:42:45 -0700

    got lateInit working better now

--- scripts/ajax.ts
+++ scripts/ajax.ts
@@ -1,6 +1,15 @@
 # Ajax specific handling if necessary
 $("/html/body") {
+  # Remove categories page for AJAXed content
   remove("./div[@id='categories']")
-  # Init jQuery Uranium widgets
-  insert("script", "$('body').Uranium('init');")
+
+  # Init jQuery Uranium widgets on homepage (may want to match path for these)
+  $$("#home") {
+    $$("[data-role='content']") {
+      insert_top("script", "$(document).ajaxComplete(function() { 
+                              $('body').Uranium('lateInit');
+                            });")
+      insert_top("script", src: asset("javascript/jquery.uranium.js"))
+    }
+  }
 }
